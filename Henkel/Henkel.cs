ad26db6785dccabf8c055cf40587017cf900ce84
Branislav Juhás <juhas.branislaw@gmail.com>
2022-11-09 21:20:38 +0100

    Updates

--- Henkel/Henkel.cs
+++ Henkel/Henkel.cs
@@ -1,4 +1,5 @@
-﻿/*  Henkel Software
+﻿/*  
+ *  Henkel Software
  *  Author: Branislav Juhás
  *  Date: 2022-11-9
  *  Description: Software for registration of faults for the KraussMaffei Corporation
@@ -6,12 +7,12 @@
  *  Last update: 2022-11-9
  *
  *  This version of program requires .NET 6.0 or higher and utilizes Terminal.Gui 1.8.2, NStack.Core 1.0.5 & the MNCA engine 1.0.2 included in the project
-*/
+ */
 
 namespace Henkel
 {
     class Program
-    {       
+    {
         static void Main(string[] args)
         {
             Interface.Initialize();
