d004101029cd70d9b2d64802be64643614e0b740
Branislav Juhás <juhas.branislaw@gmail.com>
2022-11-09 23:14:41 +0100

    Minor Updates

--- Henkel/Henkel.cs
+++ Henkel/Henkel.cs
@@ -1,5 +1,4 @@
-﻿/*  
- *  Henkel Software
+﻿/*  Henkel Software
  *  Author: Branislav Juhás
  *  Date: 2022-11-9
  *  Description: Software for registration of faults for the KraussMaffei Corporation
@@ -7,6 +6,10 @@
  *  Last update: 2022-11-9
  *
  *  This version of program requires .NET 6.0 or higher and utilizes Terminal.Gui 1.8.2, NStack.Core 1.0.5 & the MNCA engine 1.0.2 included in the project
+ *  
+ *  --  File ( Henkel.cs ) Description  --
+ *
+ * This file contains the code to initialize application.
  */
 
 namespace Henkel
--- Henkel/Interface.cs
+++ Henkel/Interface.cs
@@ -1,5 +1,12 @@
-/*
- * 
+/*  This script is a part of the Henkel project
+ *  Author: Branislav Juhás
+ *  Date: 2022-11-9
+ *  Last update: 2022-11-9
+ *  
+ *  --  File ( Interface.cs ) Description  --
+ *
+ *  This file contains the code to initialize and maintain the interface of the program.
+ *  It also contains the basic functioanlity of some elements in the interface.
  */
 
 using Terminal.Gui; // Terminal.Gui
@@ -9,9 +16,47 @@ namespace Henkel
 {
     internal class Interface
     {
+        // Initialize the interface
+
+        // Initialize the main status bar of the interface
+        private static StatusBar Status = new StatusBar(new StatusItem[] {
+            new StatusItem(Key.F4 | Key.CtrlMask, "~Ctrl F4~ Exit", () => { Application.Top.Running = false; }),
+            new StatusItem(Key.F2, "~F2~ Export", () => { }),
+            new StatusItem(Key.F5, "~F5~ Command", () => { }),
+            new StatusItem(Key.F7, "~F7~ Settings", () => { }),
+        })
+        { ColorScheme = Colors.TopLevel };
+
+        // Initialize the main input field for faults of the interface
+        private static TextField FaultInput = new TextField("")
+        {
+            X = 2,
+            Y = 1,
+            Width = Dim.Fill(2),
+            Height = 1,
+            ColorScheme = Colors.Menu
+        };
+
+        // Initialize borders for the fault input field
+        private static Label FaultInputBorder = new Label("[")
+        {
+            X = 1,
+            Y = 1,
+            ColorScheme = Colors.TopLevel
+        };
+
+        private static Label FaultInputEndBorder = new Label("]")
+        {
+            X = Pos.Right(FaultInput),
+            Y = 1,
+            ColorScheme = Colors.TopLevel
+        };
+
         public static void Initialize()
         {
-
+            Application.Init(); // Initialize the application
+            Application.Top.Add(Status, FaultInput, FaultInputBorder, FaultInputEndBorder); // Add necessary components to the application
+            Application.Run(); // Run the application
         }
     }
 }
