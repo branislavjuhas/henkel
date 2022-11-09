98784d1657e77cfd2d392a11d69b51c1f6786d8e
Branislav Juhás <juhas.branislaw@gmail.com>
2022-11-09 23:14:54 +0100

    mu

---
+++ Henkel/Properties.cs
@@ -0,0 +1,19 @@
+/*  This script is a part of the Henkel project
+ *  Author: Branislav Juhás
+ *  Date: 2022-11-9
+ *  Last update: 2022-11-9
+ *  
+ *  --  File ( Properties.cs ) Description  --
+ *
+ *  This file contains the code to initialize and maintain the properties dialog of the current
+ *  order that is being registered. You can also find all the variables storing the data of the
+ *  current ordProperties
+ */
+
+namespace Henkel
+{
+    class Properties
+    {
+
+    }
+}
